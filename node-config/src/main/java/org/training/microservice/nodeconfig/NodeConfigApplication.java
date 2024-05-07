11f3913e81fc39a5353c334f407214ac96f7d081
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-05-07 10:57:05 +0300

    day 2 1

--- node-config/src/main/java/org/training/microservice/nodeconfig/NodeConfigApplication.java
+++ node-config/src/main/java/org/training/microservice/nodeconfig/NodeConfigApplication.java
@@ -3,6 +3,7 @@ package org.training.microservice.nodeconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableConfigServer
